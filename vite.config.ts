d0a2ad27d68c1e411f20ae556fb3bb9fa19036b7
JusticeGe <1091386835@qq.com>
2022-11-06 14:49:25 +0800

    feat: add comment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         coverage: {
-            provider: 'istanbul', // or 'c8',
+            provider: 'istanbul', // or 'c8', // todo: 为啥istanbul 和c8测试出来的测试覆盖率是不一致的
             reporter: ['json', 'html']
         },
     },
